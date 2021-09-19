dd6d2fe9b92fd9755442a14d36cef9d422fa169e
Patrick <cryne@gmx.de>
2021-09-19 22:24:37 +0200

    Benchmarking

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,57 @@ mod days;
 use days::one;
 use std::fs::read_to_string;
 use crate::days::two;
+use std::time::Instant;
+
+fn fmt_time(ms: f64) -> String {
+    if ms <= 1.0 {
+        let micro_sec = ms * 1000.0;
+        return String::from(format!("{}µs", micro_sec.round()));
+    }
+
+    if ms < 1000.0 {
+        let whole_ms = ms.floor();
+        let rem_ms = ms - whole_ms;
+        return String::from(format!("{}ms ", whole_ms) + &fmt_time(rem_ms));
+    }
+
+    let sec: f64 = ms / 1000.0;
+    if sec < 60.0 {
+        let whole_sec = sec.floor();
+        let rem_ms = ms - whole_sec * 1000.0;
+
+        return format!("{}s ", whole_sec) + &fmt_time(rem_ms);
+    }
+
+    let min: f64 = sec / 60.0;
+    return format!("{}m ", min.floor()) + &fmt_time((sec % 60.0) * 1000.0);
+}
 
 fn main() {
     let one_input = read_to_string("src/inputs/one.txt").expect("Konnte Eingabe für Tag 1 nicht lesen");
+
+    let one_a_start_time = Instant::now();
     let one_a_result = one::a(&one_input);
+    let one_a_end_time = one_a_start_time.elapsed().as_secs_f64() * 1000.0;
+
+    let one_b_start_time = Instant::now();
     let one_b_result = one::b(&one_input);
-    println!("Day 1:\n A: {}\n B: {}", one_a_result, one_b_result);
+    let one_b_end_time = one_b_start_time.elapsed().as_secs_f64() * 1000.0;
+
+    println!("Day 1:\n A: {} in {}\n B: {} in {}", one_a_result, fmt_time(one_a_end_time), one_b_result, fmt_time(one_b_end_time));
+
 
     let two_input = read_to_string("src/inputs/two.txt").expect("Konnte Eingabe für Tag 2 nicht lesen");
-    let two_a_result = two::a(&two_input);
-    let two_b_result = two::b(&two_input);
-    println!("Day 2:\n A: {}\n B: {}", two_a_result, two_b_result);
 
+    let two_a_start_time = Instant::now();
+    let two_a_result = two::a(&two_input);
+    let two_a_end_time = two_a_start_time.elapsed().as_secs_f64() * 2000.0;
+
+    let two_b_start_time = Instant::now();
+    let two_b_result = two::b(&two_input);
+    let two_b_end_time = two_b_start_time.elapsed().as_secs_f64() * 2000.0;
+
+    println!("Day 2:\n A: {} in {}\n B: {} in {}", two_a_result, fmt_time(two_a_end_time), two_b_result, fmt_time(two_b_end_time));
 
 
 }
